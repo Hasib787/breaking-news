528828701defb16bed9f387aef1bbaa4c761836c
Hasib787 <mr.hasib45@gmail.com>
2021-03-03 17:49:30 +0600

    added a Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header/Header';
 import News from './components/News/News';
-
 
 
 function App() {
   return (
     <div className="container">
       <h2>React Bootstrap</h2>
+      <Header></Header>
       <News></News>
     </div>
   );
